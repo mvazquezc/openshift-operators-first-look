7924a2f72b3486506de88f31c218246c37315e90
Mario Vázquez <9841873+mvazquezc@users.noreply.github.com>
2019-03-29 18:39:12 +0100

    Update types.go

--- sources/go/types.go
+++ sources/go/types.go
@@ -4,26 +4,48 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PythonAPIHwSpec defines the desired state of PythonAPIHw
+// +k8s:openapi-gen=true
+type PythonAPIHwSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+        Size int32 `json:"size"`
+}
+
+// PythonAPIHwStatus defines the observed state of PythonAPIHw
+// +k8s:openapi-gen=true
+type PythonAPIHwStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+        ApiPods []string `json:"apiPods"`
+}
+
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// PythonAPIHw is the Schema for the pythonapihws API
+// +k8s:openapi-gen=true
+type PythonAPIHw struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PythonAPIHwSpec   `json:"spec,omitempty"`
+	Status PythonAPIHwStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PythonAPIHwList contains a list of PythonAPIHw
 type PythonAPIHwList struct {
 	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata"`
+	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []PythonAPIHw `json:"items"`
 }
 
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-type PythonAPIHw struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata"`
-	Spec              PythonAPIHwSpec   `json:"spec"`
-	Status            PythonAPIHwStatus `json:"status,omitempty"`
-}
-
-type PythonAPIHwSpec struct {
-	Size int32 `json:"size"`
-}
-type PythonAPIHwStatus struct {
-	ApiPods []string `json:"apiPods"`
+func init() {
+	SchemeBuilder.Register(&PythonAPIHw{}, &PythonAPIHwList{})
 }
